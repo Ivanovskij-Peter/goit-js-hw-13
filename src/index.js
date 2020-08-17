aa78cba0e8f42dd5d9fc92511b936ade151620f2
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-08-17 23:05:09 +0300

    добавил задачу 13 модуля

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './styles.css';
+import itemTemplate from './templates/item.hbs';
+import refs from './js/refs.js';
+//   const options = {
+//     headers: {
+//       Autorization: apiKey,
+//     },
+//   };
+
+const apiKey = '17938696-a211d439281abf74b3f685096';
+function fetchPictures() {
+  let query = 'cat';
+  const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${query}&page=1&per_page=12&key=${apiKey}`;
+  return fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      updateMarcup(itemTemplate(data));
+    });
+}
+
+fetchPictures();
+function updateMarcup(param) {
+  refs.gallery.insertAdjacentHTML('beforeend', param);
+}
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+const refs = {
+  form: document.getElementById('search-form'),
+  input: document.getElementById('input'),
+  gallery: document.querySelector('.gallery'),
+};
+export default refs;
